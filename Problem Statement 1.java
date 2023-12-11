e239ae5ff8e85dc9dcbaf524e796083bc268b0d4
Rohit <54182518+Rohit-verma01@users.noreply.github.com>
2023-12-11 12:17:06 +0530

    Proper naming convention for problem statement 1

---
+++ Problem Statement 1.java
@@ -0,0 +1,27 @@
+public class DataContainer {
+    int number;
+    double decimalNumber;
+    String text;
+
+    public void add(int number1, int number2) {
+        int sum = number1 + number2;
+        System.out.println("Result: " + sum);
+    }
+
+    public void convertToUpperCase(String text) {
+        updatedText = text.toUpperCase();
+        System.out.println("Updated String: " + updatedText);
+    }
+}
+
+public class MainApp {
+    public static void main(String[] args) {
+        DataContainer data = new DataContainer();
+        data.number = 10;
+        data.decimalNumber = 20.5;
+        data.text = "hello";
+        
+        data.add(data.number, 5);
+        data.convertToUpperCase("world");
+    }
+}
